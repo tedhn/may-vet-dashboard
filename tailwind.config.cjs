cfe6ba2b3245905168fbd6e14306ae2d53aa0b66
heinhtet naing <6927125-hhn186@users.noreply.gitlab.com>
2022-09-27 15:46:44 +0630

    initialization

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			white: "#ffffff",
+			black: "#000000",
+			teal: "#54BAB9",
+			darkGrey: "#444444",
+			lightGrey: "#ACB3C0",
+		},
+	},
+	plugins: [],
+};
